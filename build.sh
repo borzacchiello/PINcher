184e427b69a9f142c6cdbfe53ca83cd779ef58a7
Luca Borzacchiello <lucaborza@gmail.com>
2021-10-20 13:12:47 +0200

    updated pin

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -O pin.tar.gz https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.19-98425-gd666b2bee-gcc-linux.tar.gz
+wget -O pin.tar.gz https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.20-98437-gf02b61307-gcc-linux.tar.gz
 tar xf pin.tar.gz -C .
-mv pin-3.19* pin
+mv pin-3.20* pin
 rm pin.tar.gz
 
 pushd pintool
